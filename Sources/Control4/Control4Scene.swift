d243e4d8dbfa829a318ac6db9c47a539638a3561
Joshua Tessier <joshpc@gmail.com>
2022-02-25 10:24:31 -0500

    Makes Control4Scenes identifiable

--- Sources/Control4/Control4Scene.swift
+++ Sources/Control4/Control4Scene.swift
@@ -7,22 +7,22 @@
 
 import Foundation
 
-public struct Control4Scene: Codable {
-    let isActive: Bool
-    let sceneId: Int
-    let name: String
+public struct Control4Scene: Identifiable, Codable {
+    public let id: Int
+    public let isActive: Bool
+    public let name: String
     
     private enum CodingKeys: String, CodingKey {
         case name = "name"
         case isActive = "is_active"
-        case sceneId = "scene_id"
+        case id = "scene_id"
     }
 
     public init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
         
         isActive = try Bool(values.decode(String.self, forKey: .isActive)) ?? false
-        sceneId = try Int(values.decode(String.self, forKey: .sceneId)) ?? -1
+        id = try Int(values.decode(String.self, forKey: .id)) ?? -1
         name = try values.decode(String.self, forKey: .name)
     }
 }
